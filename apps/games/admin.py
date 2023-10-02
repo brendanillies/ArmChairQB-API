d6a1879b7bea4e9f685d564afa8133b3577af000
Brendan Illies <billies@paraport.com>
2023-10-02 15:22:06 -0700

    Create Admin bulk file upload for app `games`

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -1,3 +1,101 @@
 from django.contrib import admin
+from django.forms import Form, FileField
+from django.urls import path
+from django.shortcuts import render
+from .models import PlayByPlay, Schedule, Stadium
+import pandas as pd
 
 # Register your models here.
+admin.site.register(Stadium)
+
+
+class ImportCSVPlayByPlayForm(Form):
+    upload_play_by_play_file = FileField()
+
+
+class ImportCSVScheduleForm(Form):
+    upload_schedule_file = FileField()
+
+
+@admin.register(PlayByPlay, Schedule)
+class GamesAdmin(admin.ModelAdmin):
+    change_list_template = "admin/games/change_list.html"
+
+    @staticmethod
+    def __create_stadium_objects(file):
+        df = (
+            pd.read_csv(file, usecols=["stadium_id", "stadium"])
+            .rename(columns={"stadium": "name"})
+            .drop_duplicates()
+        )
+
+        objs = (Stadium(**record) for record in df.to_dict("records"))
+
+        Stadium.objects.bulk_create(objs)
+
+        return
+
+    @staticmethod
+    def __create_schedule_objects(file):
+        file.file.seek(0)
+
+        df = pd.read_csv(
+            file,
+            usecols=[
+                "game_id",
+                "season",
+                "game_type",
+                "week",
+                "gameday",
+                "weekday",
+                "gametime",
+                "away_team",
+                "home_team",
+                "location",
+                "overtime",
+                "espn",
+                "div_game",
+                "stadium_id",
+                "surface",
+                "roof",
+            ],
+            parse_dates=["gameday"],
+        ).rename(
+            columns={
+                "away_team": "away_team_id",
+                "home_team": "home_team_id",
+                "stadium_id": "stadium_id_id",
+                "espn": "espn_game_id",
+            }
+        )
+        df["home_team_id"].replace({"LA": "LAR"}, inplace=True)
+        df["away_team_id"].replace({"LA": "LAR"}, inplace=True)
+        df["overtime"].fillna(0, inplace=True)
+        df["gameday"] = pd.to_datetime(df["gameday"], format="%Y-%m-%d")
+
+        objs = (Schedule(**record) for record in df.to_dict("records"))
+
+        Schedule.objects.bulk_create(objs)
+
+        return
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path("import-csv/", self.upload_csv)]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        # TODO: If records exist, bulk_update
+        if request.method == "POST":
+            upload_schedule_file = request.FILES["upload_schedule_file"]
+            self.__create_stadium_objects(upload_schedule_file)
+            self.__create_schedule_objects(upload_schedule_file)
+
+            self.message_user(request, "Your files have been imported")
+
+        data = {
+            "playByPlayForm": ImportCSVPlayByPlayForm(),
+            "scheduleForm": ImportCSVScheduleForm(),
+        }
+
+        return render(request, "admin/games/import-csv.html", data)
--- apps/games/models.py
+++ apps/games/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.db.models import (
+    CharField,
+    IntegerField,
+    DateField,
+    TimeField,
+    ForeignKey,
+    RESTRICT,
+)
 
-# Create your models here.
+
+class Stadium(models.Model):
+    class Meta:
+        db_table = "Stadiums"
+
+    stadium_id = CharField(max_length=7, primary_key=True, editable=False)
+    stadium = CharField(max_length=70, default="", name="name", verbose_name="name")
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.stadium_id})"
+
+
+class Schedule(models.Model):
+    class Meta:
+        verbose_name_plural = "Schedule"
+        db_table = "Schedules"
+
+    game_id = models.CharField(primary_key=True, max_length=20)
+    season = IntegerField(editable=False)
+    game_type = CharField(editable=False, max_length=4)
+    week = IntegerField(editable=False)
+    gameday = DateField(editable=False)
+    weekday = CharField(editable=False, max_length=9)
+    gametime = TimeField(editable=False)
+    away_team = ForeignKey(
+        "teams.Teams", on_delete=RESTRICT, related_name="schedule_away", editable=False
+    )
+    home_team = ForeignKey(
+        "teams.Teams", on_delete=RESTRICT, related_name="schedule_home", editable=False
+    )
+    location = CharField(editable=False, max_length=4)
+    overtime = IntegerField(editable=False, default=0)
+    espn = IntegerField(
+        editable=False,
+        name="espn_game_id",
+        verbose_name="espn_game_id",
+        db_column="espn_game_id",
+    )
+    div_game = IntegerField(
+        editable=False, verbose_name="divisional_game", db_column="divisional_game"
+    )
+    stadium_id = ForeignKey(Stadium, on_delete=RESTRICT, editable=False)
+    surface = CharField(max_length=10, default="")
+    roof = CharField(max_length=8, default="")
+
+    def __str__(self) -> str:
+        return f"Week {self.week} - {self.away_team} vs. {self.home_team}"
+
+
+class PlayByPlay(models.Model):
+    class Meta:
+        verbose_name_plural = "PlayByPlay"
+        db_table = "PlayByPlay"
