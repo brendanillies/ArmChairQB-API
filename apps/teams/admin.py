32b19812508bfa72f6215e82f55330bff5061a5a
Brendan Illies <billies@paraport.com>
2023-10-10 10:31:31 -0700

    Add teams filtering

--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -13,6 +13,12 @@ class ImportCSVForm(Form):
 @admin.register(Teams)
 class TeamsAdmin(admin.ModelAdmin):
     change_list_template = "admin/teams/change_list.html"
+    list_display = ["team", "team_name", "team_division"]
+    search_fields = [
+        "team__icontains",
+        "team_name__icontains",
+        "team_division__icontains",
+    ]
 
     def get_urls(self):
         urls = super().get_urls()
@@ -22,13 +28,10 @@ class TeamsAdmin(admin.ModelAdmin):
     def upload_csv(self, request):
         # TODO: If records exist, bulk_update
         if request.method == "POST":
-           
             csv_file = request.FILES["csv_file"]
 
             df = pd.read_csv(csv_file)
-            teams = (
-                Teams(**record) for record in df.to_dict('records')
-            )
+            teams = (Teams(**record) for record in df.to_dict("records"))
             Teams.objects.bulk_create(teams)
 
             self.message_user(request, "Your csv file has been imported")
