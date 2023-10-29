5fe6514b3b7fefb3f73631c85c7d4e42ded9c9b2
Brendan Illies <brendan.illies@gmail.com>
2023-10-29 13:31:17 -0700

    Create /api/stats/player endpoint

--- apps/players/urls.py
+++ apps/players/urls.py
@@ -6,6 +6,11 @@ from players import views
 urlpatterns = [
     path("api/playerIdentifiers/", views.PlayerIdentifierList.as_view()),
     path("api/playerIdentifiers/<str:gsis_id>/", views.PlayerIdentifierDetail.as_view()),
+    path(
+        "api/stats/player/<str:gsis_id>/",
+        views.PlayerStatsDetail.as_view(),
+        name="player-stats"
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/players/views.py
+++ apps/players/views.py
@@ -1,9 +1,7 @@
 from rest_framework import generics
 
 from .models import PlayerIdentifier
-from .serializers import (
-    PlayerIdentifierSerializer,
-)
+from .serializers import PlayerIdentifierSerializer, StatsSerializer
 
 
 class PlayerIdentifierList(generics.ListAPIView):
@@ -23,3 +21,26 @@ class PlayerIdentifierDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = PlayerIdentifier.objects.all()
     serializer_class = PlayerIdentifierSerializer
     lookup_field = "gsis_id"
+
+
+class PlayerStatsDetail(generics.RetrieveAPIView):
+    """
+    Lists all statistical field instances for a player
+    """
+
+    queryset = PlayerIdentifier.objects.all()
+    serializer_class = StatsSerializer
+    lookup_field = "gsis_id"
+
+    # def get_queryset(self):
+    #     return PlayerIdentifier.objects.filter(gsis_id=self.kwargs.get("gsis_id"))
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+
+        query_params = {}
+        for param, value in self.request.query_params.items():
+            query_params[param] = value
+
+        context["query_params"] = query_params
+        return context
