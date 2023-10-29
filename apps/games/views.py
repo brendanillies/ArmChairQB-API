56202a2e4a445cc3e1f798f7163344bf26661e3f
Brendan Illies <brendan.illies@gmail.com>
2023-10-29 07:08:28 -0700

    Remove redefinition of serializer context

--- apps/games/views.py
+++ apps/games/views.py
@@ -7,12 +7,6 @@ from .serializers import ScheduleSerializer
 class ScheduleAbstractInfoList(generics.ListAPIView):
     queryset = Schedule.objects.all()
 
-    def get_serializer_context(self):
-        if "format" in self.kwargs:
-            del self.kwargs["format"]
-        context = {"request": self.kwargs}
-        return context
-
 
 class ScheduleList(ScheduleAbstractInfoList):
     """
