8eeb160d7799460b8d4673549b3a5c2e3fd75e27
Brendan Illies <brendan.illies@gmail.com>
2023-10-29 07:19:06 -0700

    Add custom model serializer for extending field definitions

--- apps/serializers.py
+++ apps/serializers.py
@@ -5,3 +5,14 @@ class FilteredListSerializer(serializers.ListSerializer):
     def to_representation(self, data):
         data = data.filter(**self.context["query_params"])
         return super().to_representation(data)
+
+
+class CustomModelSerializer(serializers.ModelSerializer):
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super().get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, 'extra_fields', None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
