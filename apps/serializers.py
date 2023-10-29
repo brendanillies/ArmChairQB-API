38aad5eeb977db1ce6d1844d219b16c9daadecf2
Brendan Illies <brendan.illies@gmail.com>
2023-10-29 07:43:16 -0700

    Add custom hyperlinked serializer

--- apps/serializers.py
+++ apps/serializers.py
@@ -8,11 +8,20 @@ class FilteredListSerializer(serializers.ListSerializer):
 
 
 class CustomModelSerializer(serializers.ModelSerializer):
-
     def get_field_names(self, declared_fields, info):
         expanded_fields = super().get_field_names(declared_fields, info)
 
-        if getattr(self.Meta, 'extra_fields', None):
+        if getattr(self.Meta, "extra_fields", None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
+
+
+class CustomHyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer):
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super().get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, "extra_fields", None):
             return expanded_fields + self.Meta.extra_fields
         else:
             return expanded_fields
