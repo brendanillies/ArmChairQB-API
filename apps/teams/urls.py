2ee468976987deb361e273538d6cde2b6ad582da
Brendan Illies <brendan.illies@gmail.com>
2023-10-29 07:04:58 -0700

    Add view names

--- apps/teams/urls.py
+++ apps/teams/urls.py
@@ -4,19 +4,22 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from teams import views
 
 urlpatterns = [
-    path("api/teams/", views.TeamList.as_view()),
-    path("api/teams/<str:team>", views.TeamList.as_view()),
+    path("api/teams/", views.TeamList.as_view(), name="team-list"),
+    path("api/teams/<str:team>", views.TeamList.as_view(), name="team-detail"),
     path(
         "api/teams/<str:team>/roster/",
-        views.TeamRoster.as_view()
+        views.TeamRoster.as_view(),
+        name="team-roster"
     ),
     path(
         "api/teams/<str:team>/depth/",
-        views.TeamDepthChart.as_view()
+        views.TeamDepthChart.as_view(),
+        name="team-depth"
     ),
     path(
         "api/teams/<str:team>/schedule/",
-        views.TeamSchedule.as_view()
+        views.TeamSchedule.as_view(),
+        name="team-schedule"
     )
 ]
 
