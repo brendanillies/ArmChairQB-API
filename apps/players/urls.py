462ec70e84a2faf8357e8e01787f54e08337fc93
Brendan Illies <brendan.illies@gmail.com>
2023-10-30 05:45:14 -0700

    Add view names

--- apps/players/urls.py
+++ apps/players/urls.py
@@ -4,8 +4,16 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from players import views
 
 urlpatterns = [
-    path("api/playerIdentifiers/", views.PlayerIdentifierList.as_view()),
-    path("api/playerIdentifiers/<str:gsis_id>/", views.PlayerIdentifierDetail.as_view()),
+    path(
+        "api/playerIdentifiers/",
+        views.PlayerIdentifierList.as_view(),
+        name="player-list"
+    ),
+    path(
+        "api/playerIdentifiers/<str:gsis_id>/",
+        views.PlayerIdentifierDetail.as_view(),
+        name="player-detail"
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
